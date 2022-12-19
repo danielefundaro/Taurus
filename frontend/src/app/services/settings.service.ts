c84221bb7d2d272dbbc0c519bf816539fe362232
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2022-12-19 02:23:14 +0100

    Save darkTheme and currentLang value into localStorage

--- frontend/src/app/services/settings.service.ts
+++ frontend/src/app/services/settings.service.ts
@@ -8,14 +8,18 @@ export class SettingsService {
     private _languageChanged = new Subject<string>();
     private _loadingChanged = new Subject<boolean>();
     private _languages: string[] = ['it', 'en'];
-    private _isDarkTheme: boolean = window.matchMedia("(prefers-color-scheme: dark)").matches || false;
+    private _isDarkTheme: boolean = !!localStorage.getItem("darkTheme") && localStorage.getItem("darkTheme") === "true" || !localStorage.getItem("darkTheme") && window.matchMedia("(prefers-color-scheme: dark)").matches;
 
     public get languages(): string[] { return this._languages; }
     public get default(): string {
-        this._languageChanged.next(this._languages[0]);
-        return this._languages[0];
+        const current = localStorage.getItem("currentLang") || this._languages[0];
+        this._languageChanged.next(current);
+        return current;
     }
-    public set currentLang(lang: string) { this._languageChanged.next(lang); }
+    public set currentLang(lang: string) {
+        this._languageChanged.next(lang);
+        localStorage.setItem("currentLang", lang);
+    }
     public get isDarkTheme(): boolean { return this._isDarkTheme; }
     public set isLoading(value: boolean) { this._loadingChanged.next(value); }
 
@@ -30,6 +34,7 @@ export class SettingsService {
     public toggleTheme(): boolean {
         this._isDarkTheme = !this._isDarkTheme;
         this.setDefalutTheme();
+        localStorage.setItem("darkTheme", String(this._isDarkTheme));
         return this._isDarkTheme;
     }
 
