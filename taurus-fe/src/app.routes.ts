3de23437067b5457d9b0f5bb146ac8df4114ed4f
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2026-02-26 22:32:31 +0100

    Remove useless files

--- taurus-fe/src/app.routes.ts
+++ taurus-fe/src/app.routes.ts
@@ -1,22 +1,17 @@
 import { Routes } from '@angular/router';
 import { canActivateAuthRole } from './app/guard/auth-role.guard';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
+import { DashboardComponent } from './app/pages/dashboard/dashboard.component';
+import { Forbidden } from './app/pages/forbidden/forbidden.component';
 import { LayoutComponent } from './app/pages/layout/layout.component';
 import { Notfound } from './app/pages/notfound/notfound.component';
 import { PreviewComponent } from './app/pages/preview/preview.component';
-import { Forbidden } from './app/pages/forbidden/forbidden.component';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: LayoutComponent,
         children: [
-            { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
+            { path: '', component: DashboardComponent },
             {
                 path: 'tenants',
                 loadChildren: () => import('./app/pages/tenants/tenants.routes'),
@@ -55,9 +50,7 @@ export const appRoutes: Routes = [
             },
         ]
     },
-    { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
     { path: 'forbidden', component: Forbidden },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
