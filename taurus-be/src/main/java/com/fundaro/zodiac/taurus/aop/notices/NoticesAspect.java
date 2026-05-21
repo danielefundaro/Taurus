ea03d7869156f7bf888d7d57d168be3588b9be7f
Daniele Fundarò <daniele.fundaro@synbrain.ai>
2026-05-21 18:09:44 +0200

    Add notices

--- taurus-be/src/main/java/com/fundaro/zodiac/taurus/aop/notices/NoticesAspect.java
+++ taurus-be/src/main/java/com/fundaro/zodiac/taurus/aop/notices/NoticesAspect.java
@@ -1,13 +1,17 @@
 package com.fundaro.zodiac.taurus.aop.notices;
 
 import com.fundaro.zodiac.taurus.domain.enumeration.StateEnum;
+import com.fundaro.zodiac.taurus.service.AlbumsService;
+import com.fundaro.zodiac.taurus.service.CommonOpenSearchService;
 import com.fundaro.zodiac.taurus.service.NoticesService;
+import com.fundaro.zodiac.taurus.service.TracksService;
 import com.fundaro.zodiac.taurus.service.dto.*;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
@@ -17,106 +21,132 @@ public class NoticesAspect {
 
     private final NoticesService noticesService;
 
-    public NoticesAspect(NoticesService noticesService) {
+    private final AlbumsService albumsService;
+
+    private final TracksService tracksService;
+
+    public NoticesAspect(NoticesService noticesService, AlbumsService albumsService, TracksService tracksService) {
         this.noticesService = noticesService;
+        this.albumsService = albumsService;
+        this.tracksService = tracksService;
     }
 
     @Around("execution(public * com.fundaro.zodiac.taurus.service.impl.AlbumsServiceImpl.save()) || " +
         "execution(public * com.fundaro.zodiac.taurus.service.impl.TracksServiceImpl.save())")
     public Object addNoticesOnSave(ProceedingJoinPoint joinPoint) throws Throwable {
-        StateAndTokenResult result = getStateAndToken(joinPoint);
+        AbstractAuthenticationToken abstractAuthenticationToken = getAbstractAuthenticationToken(joinPoint);
+        FinalValue finalValue = getFinalValue(joinPoint);
 
-        if (result.stateOpt() != null && result.tokenOpt() != null) {
+        if (finalValue.stateOpt() != null && abstractAuthenticationToken != null) {
             String className = joinPoint.getTarget().getClass().getSimpleName();
-            String name, namePublic, message, messagePublic;
+            String name, nameWithStatus, message, messageWithStatus;
 
             if (className.toLowerCase().contains("album")) {
                 name = "Nuovo album creato";
-                namePublic = "Nuovo album pubblicato";
-                message = String.format("L'album \"%s\" è stato creato", result.stateOpt().getName());
-                messagePublic = String.format("Il nuovo album \"%s\" è stato pubblicato", result.stateOpt().getName());
+                nameWithStatus = "Nuovo album pubblicato";
+                message = String.format("L'album \"%s\" è stato creato", finalValue.stateOpt().getName());
+                messageWithStatus = String.format("Il nuovo album \"%s\" è stato pubblicato", finalValue.stateOpt().getName());
             } else {
                 name = "Nuova traccia creata";
-                namePublic = "Nuova traccia pubblicata";
-                message = String.format("La traccia \"%s\" è stata creata", result.stateOpt().getName());
-                messagePublic = String.format("La nuova traccia \"%s\" è stata pubblicata", result.stateOpt().getName());
+                nameWithStatus = "Nuova traccia pubblicata";
+                message = String.format("La traccia \"%s\" è stata creata", finalValue.stateOpt().getName());
+                messageWithStatus = String.format("La nuova traccia \"%s\" è stata pubblicata", finalValue.stateOpt().getName());
             }
 
-            if (result.stateOpt().getState() == StateEnum.PUBLIC) {
-                noticesService.addNoticeWholeTenant(namePublic, messagePublic, result.tokenOpt()).block();
+            if (finalValue.stateOpt().getState() == StateEnum.PUBLIC) {
+                noticesService.addNoticeWholeTenant(name, message, abstractAuthenticationToken).block();
             } else {
-                noticesService.addNoticesExcludeRoleUsers(name, message, result.tokenOpt()).block();
+                noticesService.addNoticesExcludeRoleUsers(nameWithStatus, messageWithStatus, abstractAuthenticationToken).block();
             }
         }
 
         return joinPoint.proceed();
     }
 
-    @Around("execution(private * com.fundaro.zodiac.taurus.service.impl.CommonOpenSearchServiceImpl.updateDto())")
+    @Around("execution(private * com.fundaro.zodiac.taurus.service.impl.AlbumsServiceImpl.update()) || " +
+        "execution(public * com.fundaro.zodiac.taurus.service.impl.TracksServiceImpl.update()) || " +
+        "execution(public * com.fundaro.zodiac.taurus.service.impl.AlbumsServiceImpl.partialUpdate()) || " +
+        "execution(public * com.fundaro.zodiac.taurus.service.impl.TracksServiceImpl.partialUpdate())")
     public Object addNoticesOnUpdate(ProceedingJoinPoint joinPoint) throws Throwable {
-        StateAndTokenResult result = getStateAndToken(joinPoint);
-        Object proceed = joinPoint.proceed();
-        StateFieldOpenSearchDTO newStateOpt = null;
+        AbstractAuthenticationToken abstractAuthenticationToken = getAbstractAuthenticationToken(joinPoint);
+        String id = getId(joinPoint);
+        FinalValue finalValue = getFinalValue(joinPoint);
 
-        if (proceed instanceof StateFieldOpenSearchDTO) {
-            newStateOpt = (StateFieldOpenSearchDTO) proceed;
-        }
-
-        if (result.stateOpt() != null && result.tokenOpt() != null && newStateOpt != null) {
+        if (id != null && abstractAuthenticationToken != null && finalValue.stateOpt() != null) {
             String className = joinPoint.getTarget().getClass().getSimpleName();
-            String name, namePublic, message, messagePublic;
+            String nameForUsers, namePublic, nameUpdate, nameDelete, messageForUsers, messagePublic, messageUpdate, messageDelete;
+            StateFieldOpenSearchDTO oldState = getStateFieldOpenSearchDTO(className, id, abstractAuthenticationToken);
 
             if (className.toLowerCase().contains("album")) {
-                name = "Album aggiornato";
+                nameForUsers = "Nuovo album creato";
                 namePublic = "Album pubblicato";
-                message = String.format("L'album \"%s\" è stato aggiornato", newStateOpt.getName());
-                messagePublic = String.format("L'album \"%s\" è stato pubblicato", newStateOpt.getName());
+                nameUpdate = "Album aggiornato";
+                nameDelete = "Album rimosso";
+                messageForUsers = String.format("L'album \"%s\" è stato creato", finalValue.stateOpt().getName());
+                messagePublic = String.format("L'album \"%s\" è stato pubblicato", finalValue.stateOpt().getName());
+                messageUpdate = String.format("L'album \"%s\" è stato aggiornato", finalValue.stateOpt().getName());
+                messageDelete = String.format("L'album \"%s\" è stato rimosso", finalValue.stateOpt().getName());;
             } else {
-                name = "Traccia aggiornata";
+                nameForUsers = "Nuova traccia creata";
                 namePublic = "Traccia pubblicata";
-                message = String.format("La traccia \"%s\" è stata aggiornata", result.stateOpt().getName());
-                messagePublic = String.format("La traccia \"%s\" è stata pubblicata", result.stateOpt().getName());
+                nameUpdate = "Traccia aggiornata";
+                nameDelete = "Traccia rimossa";
+                messageForUsers = String.format("La traccia \"%s\" è stata creata", finalValue.stateOpt().getName());
+                messagePublic = String.format("La traccia \"%s\" è stata pubblicata", finalValue.stateOpt().getName());
+                messageUpdate = String.format("La traccia \"%s\" è stata aggiornata", finalValue.stateOpt().getName());
+                messageDelete = String.format("La traccia \"%s\" è stata rimossa", finalValue.stateOpt().getName());
             }
 
-            if (newStateOpt.getState() == StateEnum.PUBLIC) {
-                if (result.stateOpt().getState() != StateEnum.PUBLIC) {
-                    noticesService.addNoticeWholeTenant(namePublic, messagePublic, result.tokenOpt()).block();
+            if (oldState.getState() != StateEnum.PUBLIC) {
+                if (finalValue.stateOpt.getState() == StateEnum.PUBLIC) {
+                    noticesService.addNoticeOnlyRoleUsers(nameForUsers, messageForUsers, abstractAuthenticationToken)
+                        .then(Mono.fromCallable(() -> noticesService.addNoticesExcludeRoleUsers(namePublic, messagePublic, abstractAuthenticationToken))).block();
                 } else {
-                    noticesService.addNoticeWholeTenant(name, message, result.tokenOpt()).block();
+                    noticesService.addNoticesExcludeRoleUsers(nameUpdate, messageUpdate, abstractAuthenticationToken).block();
                 }
             } else {
-                noticesService.addNoticesExcludeRoleUsers(name, message, result.tokenOpt()).block();
+                if (finalValue.stateOpt.getState() == StateEnum.PUBLIC) {
+                    noticesService.addNoticeWholeTenant(nameUpdate, messageUpdate, abstractAuthenticationToken).block();
+                } else {
+                    noticesService.addNoticeOnlyRoleUsers(nameDelete, messageDelete, abstractAuthenticationToken)
+                        .then(Mono.fromCallable(() -> noticesService.addNoticesExcludeRoleUsers(nameUpdate, messageUpdate, abstractAuthenticationToken))).block();
+                }
             }
         }
 
-        return proceed;
+        return finalValue.proceed();
     }
 
     @Around("execution(public * com.fundaro.zodiac.taurus.service.impl.AlbumsServiceImpl.delete()) || " +
         "execution(public * com.fundaro.zodiac.taurus.service.impl.TracksServiceImpl.delete())")
     public Object addNoticesOnDelete(ProceedingJoinPoint joinPoint) throws Throwable {
-        StateAndTokenResult result = getStateAndToken(joinPoint);
+        AbstractAuthenticationToken abstractAuthenticationToken = getAbstractAuthenticationToken(joinPoint);
+        String id = getId(joinPoint);
+        FinalValue finalValue = getFinalValue(joinPoint);
 
-        if (result.stateOpt() != null && result.tokenOpt() != null) {
+        if (id != null && abstractAuthenticationToken != null) {
             String className = joinPoint.getTarget().getClass().getSimpleName();
             String name, message;
+            StateFieldOpenSearchDTO currentStateObj = getStateFieldOpenSearchDTO(className, id, abstractAuthenticationToken);
 
-            if (className.toLowerCase().contains("album")) {
-                name = "Album rimosso";
-                message = String.format("L'album \"%s\" è stato rimosso", result.stateOpt().getName());
-            } else {
-                name = "Traccia rimossa";
-                message = String.format("La traccia \"%s\" è stata rimossa", result.stateOpt().getName());
-            }
+            if (currentStateObj != null) {
+                if (className.toLowerCase().contains("album")) {
+                    name = "Album rimosso";
+                    message = String.format("L'album \"%s\" è stato rimosso", currentStateObj.getName());
+                } else {
+                    name = "Traccia rimossa";
+                    message = String.format("La traccia \"%s\" è stata rimossa", currentStateObj.getName());
+                }
 
-            if (result.stateOpt().getState() == StateEnum.PUBLIC) {
-                noticesService.addNoticeWholeTenant(name, message, result.tokenOpt()).block();
-            } else {
-                noticesService.addNoticesExcludeRoleUsers(name, message, result.tokenOpt()).block();
+                if (currentStateObj.getState() == StateEnum.PUBLIC) {
+                    noticesService.addNoticeWholeTenant(name, message, abstractAuthenticationToken).block();
+                } else {
+                    noticesService.addNoticesExcludeRoleUsers(name, message, abstractAuthenticationToken).block();
+                }
             }
         }
 
-        return joinPoint.proceed();
+        return finalValue.proceed();
     }
 
     @Around("execution(public * com.fundaro.zodiac.taurus.service.impl.InstrumentsServiceImpl.save()) || " +
@@ -199,7 +229,7 @@ public class NoticesAspect {
             String className = joinPoint.getTarget().getClass().getSimpleName();
             String methodName = joinPoint.getSignature().getName();
 
-            if (className.toLowerCase().contains("instrument")) {
+            if (className.toLowerCase().contains("tenant")) {
                 ResultNameMessage resultNameMessage = switch (methodName) {
                     case "save":
                         yield new ResultNameMessage("Nuovo tenant", String.format("Il tenant \"%s\" e codice \"%s\" è stato aggiunto", tenantOpt.getName(), tenantOpt.getCode()));
@@ -221,9 +251,21 @@ public class NoticesAspect {
         return joinPoint.proceed();
     }
 
-    private static StateAndTokenResult getStateAndToken(ProceedingJoinPoint joinPoint) {
-        StateFieldOpenSearchDTO stateOpt = (StateFieldOpenSearchDTO) getCommonFieldsOpenSearchDTO(joinPoint);
-        return new StateAndTokenResult(stateOpt, getAbstractAuthenticationToken(joinPoint));
+    private static FinalValue getFinalValue(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object proceed = joinPoint.proceed();
+        StateFieldOpenSearchDTO stateOpt = null;
+
+        if (proceed instanceof StateFieldOpenSearchDTO) {
+            stateOpt = (StateFieldOpenSearchDTO) proceed;
+        }
+
+        return new FinalValue(proceed, stateOpt);
+    }
+
+    private StateFieldOpenSearchDTO getStateFieldOpenSearchDTO(String className, String id, AbstractAuthenticationToken abstractAuthenticationToken) {
+        CommonOpenSearchService s = className.toLowerCase().contains("album") ? albumsService : tracksService;
+        StateFieldOpenSearchDTO currentStateObj = (StateFieldOpenSearchDTO) s.findOne(id, abstractAuthenticationToken).block();
+        return currentStateObj;
     }
 
     private static CommonFieldsOpenSearchDTO getCommonFieldsOpenSearchDTO(ProceedingJoinPoint joinPoint) {
@@ -240,7 +282,14 @@ public class NoticesAspect {
             .findFirst().orElse(null);
     }
 
-    private record StateAndTokenResult(StateFieldOpenSearchDTO stateOpt, AbstractAuthenticationToken tokenOpt) {
+    private static String getId(ProceedingJoinPoint joinPoint) {
+        return Arrays.stream(joinPoint.getArgs())
+            .filter(arg -> arg instanceof String)
+            .map(arg -> (String) arg)
+            .findFirst().orElse(null);
+    }
+
+    private record FinalValue(Object proceed, StateFieldOpenSearchDTO stateOpt) {
     }
 
     private record ResultNameMessage(String name, String message) {
